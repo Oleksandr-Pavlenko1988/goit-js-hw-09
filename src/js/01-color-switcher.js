6393fe6742fc570888cf088d33d7c0125f277a59
Oleksandr <elvenmonuskrypt@gmail.com>
2023-04-01 21:09:37 +0300

    homework is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    isActive: false,
+  };
 
+  refs.start.addEventListener('click', handleStartBtn);
+  refs.stop.addEventListener('click', handleStopBtn);
+
+  let intervalId = null;
+
+  function handleStartBtn () {
+    refs.isActive = true;
+    refs.start.disabled = true;
+      intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+  };
+
+  function handleStopBtn () {
+    refs.isActive = false;
+      refs.start.disabled = false;
+      clearInterval(intervalId);
+  };
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handlerSubmit);
+
+let delayMs = null;
+let step = null;
+let amount = null;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, (delayMs += step));
+  });
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  delayMs = Number(formEl.delay.value);
+  step = Number(formEl.step.value);
+  amount = Number(formEl.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delayMs)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  formEl.reset();
 }
