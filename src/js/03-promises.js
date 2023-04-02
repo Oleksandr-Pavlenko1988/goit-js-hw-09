49b7c16e7551196cf47582ec30b5b53ee75bd8ec
Oleksandr <elvenmonuskrypt@gmail.com>
2023-04-02 18:00:54 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,7 @@ const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', handlerSubmit);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;  
+  const shouldResolve = Math.random() > 0.3;
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -14,15 +14,15 @@ function createPromise(position, delay) {
       } else {
         reject({ position, delay });
       }
-    }, (delayMs += step));
+    }, delay);
   });
 }
 
 function handlerSubmit(e) {
   e.preventDefault();
-  delayMs = Number(e.target.delay.value);
-  step = Number(e.target.step.value);
-  amount = Number(e.target.amount.value);
+  let delayMs = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delayMs)
@@ -32,6 +32,7 @@ function handlerSubmit(e) {
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+    delayMs += step;
   }
   e.target.reset();
 }
